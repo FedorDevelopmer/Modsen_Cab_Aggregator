b3373ae1a8857922a52e3ff6b4237baa0a1d1eb6
Fyodor <fedor.sap@gmail.com>
2024-10-16 03:35:14 +0300

    Develop Branch Fixes

--- src/passenger-service/src/test/java/com/modsen/software/passenger/repository/PassengerRepositoryTest.java
+++ src/passenger-service/src/test/java/com/modsen/software/passenger/repository/PassengerRepositoryTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.annotation.DirtiesContext;
@@ -20,6 +21,7 @@ import java.util.Optional;
 
 @DataJpaTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 public class PassengerRepositoryTest {
 
